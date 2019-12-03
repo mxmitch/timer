cd315ef5b0d62dc197d11fd21970d72900f4cab7
Mitch Lum <mklum88@gmail.com>
2019-12-03 23:12:38 +0000

    function to implement a simple Alarm Clock/Timer app. Pairing with @AliceMathews

---
+++ timer1.js
@@ -0,0 +1,11 @@
+const alarms = process.argv.slice(2);​​
+
+for (const alarm of alarms) {
+  console.log('check');
+  if (alarm >= 0) {
+    console.log('check2');
+    setTimeout(() => {
+      process.stdout.write('\x07');
+    }, alarm * 1000); //convert to ms
+  }
+}
